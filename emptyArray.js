0129f88fd8adf53b8b67cdc23146a5863e8ecb43
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-03-24 21:52:27 +0530

    Add files via upload

---
+++ emptyArray.js
@@ -0,0 +1,21 @@
+// let arr = [1, 2, 3, 4];
+// arr.length = 0;
+// console.log("@SN ", arr);
+
+// let arr = [1, 2, 3, 4];
+// arr = [];
+// console.log("@SN ", arr);
+
+// let arr = [1, 2, 3, 4];
+// arr.splice(0, arr.length);
+// console.log("@SN ", arr);
+
+// let arr = [1, 2, 3, 4];
+// while (arr.length > 0) {
+//   arr.pop();
+// }
+// console.log("@SN ", arr);
+
+let arr = [1, 2, 3, 4];
+let rse = arr.slice(2, 3);
+console.log("@SN ", rse);
